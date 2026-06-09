ec622e7e47cc0a2f45d2b80fb5d72ef25c5dc527
alexmexico1 <alex.obi@miva.edu.ng>
2026-06-09 10:35:36 -0700

    fix vite chunk assets loading path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
+    outDir: 'dist',
     rollupOptions: {
       output: {
         manualChunks(id) {
-          // Check if the dependency originates from node_modules
           if (id.includes('node_modules')) {
-            // Isolate recharts and d3 modules into their own separate file
             if (id.includes('recharts') || id.includes('d3')) {
               return 'vendor-charts';
             }
-            // Put other standard core dependencies somewhere else
             return 'vendor-core';
           }
         },
