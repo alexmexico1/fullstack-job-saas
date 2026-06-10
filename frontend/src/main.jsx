fc49379a06ec5af7a7fe0cdcb7cf59a6017fc997
alexmexico1 <alex.obi@miva.edu.ng>
2026-06-09 17:19:11 -0700

    fix: ensure old js file is deleted and main imports jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./services/authService.js"; 
+import { AuthProvider } from "./services/authService.jsx";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
