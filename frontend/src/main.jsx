0e12518d609c5ccaaa68b069cba91f0083a2dbbd
alexmexico1 <alex.obi@miva.edu.ng>
2026-08-15 16:29:44 -0700

    Upgrade TaskFlow to premium SaaS design system

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,22 @@
+
+/* TASKFLOW_URL_CLEANUP */
+if (typeof window !== "undefined") {
+  const url = new URL(window.location.href);
+
+  if (url.searchParams.has("utm_source")) {
+    url.searchParams.delete("utm_source");
+    window.history.replaceState(
+      {},
+      document.title,
+      url.pathname +
+        (url.searchParams.toString()
+          ? "?" + url.searchParams.toString()
+          : "") +
+        url.hash
+    );
+  }
+}
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -6,6 +25,7 @@ import { ThemeProvider } from "./context/ThemeContext.jsx";
 import App from "./App.jsx";
 import "./index.css";
 import "./taskflow-layout.css";
+import "./taskflow-premium.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
